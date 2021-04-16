473d489b46cc58fdc5d4a8bc6428c45cb2debe2f
mehmetsekeroglu <mehmetsekeroglu2019@gmail.com>
2021-04-16 11:34:39 +0200

    css

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import {useState} from "react"
 import Forecast from '../Forecast/Forecast';
 
 export default function HomePage() {
-  const [city, setCity] = useState("")
+const [city, setCity] = useState("")
 const API_KEY = "5fec1a9312514f5ab84aab4ed30a0beb"
 const API_URL = `https://api.weatherbit.io/v2.0/current?&city=${city}&key=${API_KEY}&include=minutely`;
       return (
